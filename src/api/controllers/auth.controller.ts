4a40b2ea3a49252f986eaf9a48ff22174f1d95ad
gskim <kiseon1987@gmail.com>
2024-07-07 13:57:04 +0900

    .

--- src/api/controllers/auth.controller.ts
+++ src/api/controllers/auth.controller.ts
@@ -21,8 +21,9 @@ export class AuthController {
 	async postSignIn(@Body() body: PostAuthSignInRequest) {
 		const user = await this.userService.getUserByAccount(body.account)
 		if (!user) {
-			throw new UnauthorizedException('가입되어있지않은 계정입니다')
+			throw new UnauthorizedException('가입되어있지않은 계정입니다.')
 		}
+		const token = await this.userService.comparePassword()
 		return
 	}
 
@@ -31,9 +32,9 @@ export class AuthController {
 	@CommonResponse({ type: PostAuthSignUpResponse })
 	async postSignUp(@Body() body: PostAuthSignUpRequest) {
 		const user = await this.userService.createUser(body.account, body.email, body.password)
-		const toekn = await this.userService.makeToken(user)
+		const token = await this.userService.makeToken(user)
 		return PostAuthSignUpResponse.builder()
-			.token(toekn)
+			.token(token)
 			.build()
 	}
 
