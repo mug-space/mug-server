e0492b219f744c6d3a412d952137a609ce9b6b80
gskim <kiseon1987@gmail.com>
2024-07-14 00:20:09 +0900

    toss payment confirm

--- src/api/controllers/payment.controller.ts
+++ src/api/controllers/payment.controller.ts
@@ -1,13 +1,20 @@
-import { Body, Controller, Get, Inject, NotFoundException, Post, UnauthorizedException, UseGuards } from '@nestjs/common'
+import { HttpService } from '@nestjs/axios'
+import { Body, Controller, Get, Inject, NotFoundException, Post, Query, UnauthorizedException, UseGuards } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { ApiOperation, ApiTags } from '@nestjs/swagger'
 import { JwtAuthGuard } from 'src/common/auth/jwt.guard'
 import { CurrentUser } from 'src/common/custom.decorator'
 import { CommonResponse } from 'src/common/response'
+import { UserModel } from '../dtos/models/user.model'
+
+const secretKey = 'test_gsk_docs_OaPz8L5KdmQXkzRz3y47BMw6'
 
 @Controller('payments')
 @ApiTags('Billing')
 export class PaymentController {
+
+	@Inject()
+	private readonly httpService: HttpService
 
 	// TODO: 결제 목록
 	async getPaymentList() {
@@ -15,8 +22,25 @@ export class PaymentController {
 	}
 
 	// TODO: 결제 승인
-	async confirmPayment() {
-
+	@Post('confirm')
+	async confirmPayment(@CurrentUser() user: UserModel | null, @Body() body: { paymentKey: string, orderId: string, amount: number }) {
+		if (!user) {
+			throw new UnauthorizedException('Required Login')
+		}
+		const { paymentKey, orderId, amount } = body
+		const encryptedSecretKey =
+    'Basic ' + Buffer.from(secretKey + ':').toString('base64')
+		const response = await this.httpService.post('https://api.tosspayments.com/v1/payments/confirm',
+			JSON.stringify({ orderId, amount, paymentKey }),
+			{ headers: {
+				Authorization: encryptedSecretKey,
+				'Content-Type': 'application/json',
+			} }
+		).toPromise()
+		if (response) {
+			return response.data
+		}
+		return null
 	}
 
 }
