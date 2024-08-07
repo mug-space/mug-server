bbb8ec16c0fb71cbe208f2fcc7746faee9be89b3
gskim <kiseon1987@gmail.com>
2024-08-07 22:21:52 +0900

    inquiry user id

--- src/api/controllers/inquiry.controller.ts
+++ src/api/controllers/inquiry.controller.ts
@@ -1,14 +1,15 @@
-import { Body, Controller, Get, Inject, NotFoundException, Param, Post, UnauthorizedException, UseGuards } from '@nestjs/common'
-import { ConfigService } from '@nestjs/config'
+import { Body, Controller, Get, Inject, NotFoundException, Param, Post, UseGuards } from '@nestjs/common'
 import { ApiOperation, ApiTags } from '@nestjs/swagger'
 import { JwtAuthGuard } from 'src/common/auth/jwt.guard'
 import { CurrentUser } from 'src/common/custom.decorator'
 import { CommonResponse } from 'src/common/response'
 import { GetInquiryDetailResponse, GetInquiryListResponse, PostInquiryAddRequest } from '../dtos/inquiry.dto'
+import { UserModel } from '../dtos/models/user.model'
 import { InquiryService } from '../services/inquiry.service'
 
 @Controller('inquiries')
 @ApiTags('Inquiry')
+@UseGuards(JwtAuthGuard)
 export class InquiryController {
 
 	@Inject()
@@ -17,16 +18,16 @@ export class InquiryController {
 	@Post()
 	@ApiOperation({ summary: '문의 등록' })
 	@CommonResponse({ type: Boolean })
-	async addInquiry(@Body() body: PostInquiryAddRequest) {
-		await this.inquiryService.addInquiry(1, body.title, body.content)
+	async addInquiry(@CurrentUser() user: UserModel, @Body() body: PostInquiryAddRequest) {
+		await this.inquiryService.addInquiry(user.id, body.title, body.content)
 		return true
 	}
 
 	@Get()
 	@ApiOperation({ summary: '문의 목록' })
 	@CommonResponse({ type: GetInquiryListResponse })
-	async getInquiryList() {
-		const inquiryList = await this.inquiryService.getInquiryList(1)
+	async getInquiryList(@CurrentUser() user: UserModel) {
+		const inquiryList = await this.inquiryService.getInquiryList(user.id)
 		return GetInquiryListResponse.builder()
 			.inquiries(inquiryList)
 			.build()
@@ -35,8 +36,8 @@ export class InquiryController {
 	@Get(':id(\\d+)')
 	@ApiOperation({ summary: '문의 상세' })
 	@CommonResponse({ type: GetInquiryDetailResponse })
-	async getInquiryDetail(@Param('id') id: number) {
-		const inquiry = await this.inquiryService.getInquiry(1, id)
+	async getInquiryDetail(@Param('id') id: number, @CurrentUser() user: UserModel) {
+		const inquiry = await this.inquiryService.getInquiry(user.id, id)
 		if (!inquiry) {
 			throw new NotFoundException('not found inquiry')
 		}
